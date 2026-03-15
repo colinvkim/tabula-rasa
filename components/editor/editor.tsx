f87ae495fc40ad6f1829b4ce12353af1486b64f5
Colin Kim <121141519+colinvkim@users.noreply.github.com>
2026-03-15 13:39:07 -0700

    feat: add placeholder

--- components/editor/editor.tsx
+++ components/editor/editor.tsx
@@ -2,11 +2,14 @@
 
 import { useEditor, EditorContent } from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
+import { Placeholder } from "@tiptap/extensions"
 
 const Tiptap = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      StarterKit,
+      Placeholder.configure({ placeholder: "Start typing..." }),
+    ],
     // Don't render immediately on the server to avoid SSR issues
     immediatelyRender: false,
   })
